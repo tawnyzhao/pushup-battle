6b13b2d361e9e8a00573ee90f7e1e727cb96c84b
Tony <tony.zhao@lcbo.com>
2021-01-16 13:06:35 -0800

    Add server functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 import Cam from './components/Cam';
-
+import { useState } from 'react';
 import './App.css';
+import { io } from 'socket.io-client';
+const socket = io('http://localhost:9000');
 
 function App() {
+  const [score, updateScore] = useState({});
+  
+  socket.on("connect", () => {
+    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+  });
+  socket.on("pull score", updateScore);
+
+  
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <Cam></Cam>
+      <div style={{color:'black'}}onClick={() => {socket.emit('push score', 32);}}> 
+      push 
+      </div>
+      {Object.entries(score)}
     </div>
   );
 }
