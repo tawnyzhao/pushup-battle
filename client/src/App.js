fc79ce6e82aa1bacf4810d22254c78da02231160
Tony Zhao <tawnyzhao@gmail.com>
2021-01-16 20:44:22 -0800

    Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,35 @@
 import Cam from "./components/Cam";
 import { useState } from "react";
 import "./App.css";
-import { io } from "socket.io-client";
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import Lobby from "./components/Lobby";
+import Landing from './components/Landing';
 
 function App() {
+  let [session, setSession] = useState({});
+  console.log(session);
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <Lobby></Lobby>
-    </div>
+    <Router> 
+      <div className="App">
+        <Switch>
+          <Route path="/:id">
+            <Lobby session={session}/>
+          </Route>
+          <Route exact path="/">
+            {
+              Object.keys(session).length != 0 ? 
+              <Redirect to={`/${session.roomID}`} /> :
+              <Landing updateSession={setSession}/>
+            }
+          </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
